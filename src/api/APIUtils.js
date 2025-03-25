6d110add5a586e7078c1347c173cdc679b93e09d
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-03-25 14:46:25 +0530

    Removed code and added dynamic css classes

--- src/api/APIUtils.js
+++ src/api/APIUtils.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://flipkart-email-mock.now.sh/"
+export const API_BASE_URL = "https://flipkart-email-mock.now.sh/";
--- src/components/EmailApp.jsx
+++ src/components/EmailApp.jsx
@@ -1,42 +1,128 @@
 import { useEffect, useState } from "react";
-import EmailItem from "./EmailItem";
+import EmailItem from "./EmaiIItem";
 import Header from "./Header";
+import EmailViewer from "./EmailViewer";
+import Loading from "./Loading";
 
 const EmailApp = () => {
   const [emailList, setEmailList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [selected, setSelected] = useState();
+  const [selectedEmail, setSelectedEmail] = useState(null);
+  const [selectNav, setSelectNav] = useState("Unread");
 
   useEffect(() => {
-    const fetchData = async () => {
+    if (!selected) return;
+
+    const fetchSelectedData = async (email) => {
+      const res = await fetch(
+        `https://flipkart-email-mock.now.sh/?id=${email?.id}`
+      );
+      const data = await res.json();
+      setSelectedEmail({ ...email, body: data.body });
+    };
+
+    fetchSelectedData(selected);
+  }, [selected]);
+
+  useEffect(() => {
+    const fetchData = async (page) => {
       try {
-        const res = await fetch("https://flipkart-email-mock.now.sh");
+        const res = await fetch(
+          `https://flipkart-email-mock.now.sh/?page=${page}`
+        );
         const data = await res.json();
-        setEmailList(data);
+        const newRes = data?.list?.map((res) => {
+          return { ...res, ...{ isRead: false, isFavorite: false } };
+        });
+
+        setEmailList(newRes);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
     };
-    fetchData();
+    fetchData(1);
   }, []);
+
+  useEffect(() => {
+    if (!selectNav) return;
+
+    setSelectedEmail(null);
+    setSelected(null);
+
+    setEmailList((prevEmails) => {
+      return prevEmails.map((email) => {
+        if (selectNav === "Read") {
+          return { ...email, hidden: !email.isRead };
+        } else if (selectNav === "Unread") {
+          return { ...email, hidden: email.isRead };
+        } else if (selectNav === "Favorites") {
+          return { ...email, hidden: !email.isFavorite };
+        } else {
+          return { ...email, hidden: false };
+        }
+      });
+    });
+  }, [selectNav]);
+
+  // Function to toggle favorite status
+  const toggleFavorite = (emailId) => {
+    setEmailList((prevEmails) =>
+      prevEmails.map((email) =>
+        email.id === emailId
+          ? { ...email, isFavorite: !email.isFavorite }
+          : email
+      )
+    );
+
+    if (selectedEmail?.id === emailId) {
+      setSelectedEmail((prev) => ({ ...prev, isFavorite: !prev.isFavorite }));
+    }
+  };
+
+  //
+  const handleReadEmail = (email) => {
+    setEmailList((prevEmails) =>
+      prevEmails.map((prevEmail) =>
+        prevEmail.id === email.id ? { ...prevEmail, isRead: true } : prevEmail
+      )
+    );
+    setSelected(email);
+  };
 
   return (
     <div className="container mt-10 mx-auto">
       {/* Headers */}
-      <Header />
+      <Header selectNav={selectNav} setSelectNav={setSelectNav} />
       {/* List of email */}
-      <main>
-        {emailList?.list?.map((email) => (
-          <EmailItem
-            bgColor="bg-[var(--real-background)]"
-            key={email.id}
-            email={email}
-          />
-        ))}
-        {/* <EmailItem bgColor="bg-white" />
-        <EmailItem bgColor="bg-white" />
-        <EmailItem bgColor="bg-white" />
-        <EmailItem bgColor="bg-white" />
-        <EmailItem bgColor="bg-white" />
-        <EmailItem bgColor="bg-white" /> */}
+      <main
+        className={`flex gap-10 w-full ${selected ? "" : "justify-center"}`}
+      >
+        {/* Email List (40% width) */}
+        <div className={`${selected ? "w-2/5" : "w-full"} mb-24`}>
+          {emailList?.map((email) => (
+            <EmailItem
+              key={email.id}
+              email={email}
+              handleReadEmail={handleReadEmail}
+              selected={selected}
+            />
+          ))}
+        </div>
+
+        {/* Loading Indicator */}
+        {loading && <Loading loading={loading} />}
+
+        {/* Email Viewer (60% width) */}
+        {!loading && selectedEmail && (
+          <div className="w-3/5">
+            <EmailViewer
+              selectedEmail={selectedEmail}
+              toggleFavorite={toggleFavorite}
+            />
+          </div>
+        )}
       </main>
     </div>
   );
