e0f36833ef0a689944bfc051ffe26afd3bc1d707
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-03-25 15:18:08 +0530

    Dynamically added navbar buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,23 @@
-const Header = () => {
+const Header = ({ selectNav, setSelectNav }) => {
+  const buttons = ["Unread", "Read", "Favorites"];
+
   return (
     <header>
       <nav className="flex gap-4">
-        <span className="">Filter By:</span>
-        <button>Unread</button>
-        <button className="border rounded-full px-3 text-md border-gray-300 bg-[var(--filter-btn)] font-light">
-          Read
-        </button>
-        <button>Favorites</button>
+        <span className="mr-8">Filter By:</span>
+        {buttons.map((button, index) => (
+          <button
+            key={index}
+            className={`rounded-full px-3 text-md ${
+              selectNav === button
+                ? "bg-[var(--filter-btn)] border border-gray-300  font-light"
+                : ""
+            }`}
+            onClick={() => setSelectNav(button)}
+          >
+            {button}
+          </button>
+        ))}
       </nav>
     </header>
   );
