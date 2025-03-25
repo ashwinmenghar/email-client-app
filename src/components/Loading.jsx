0710cd68072a55d37cf3cd06289c87136aee93d8
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-03-25 15:17:03 +0530

    absolute css class added for loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import { ClipLoader } from "react-spinners";
 
 const Loading = ({ loading }) => {
   return (
-    <div className=" h-[100vh] flex justify-center items-center">
+    <div className="h-[100vh] flex justify-center items-center z-50 absolute left-[50%]">
       <ClipLoader
         loading={loading}
         size={100}
